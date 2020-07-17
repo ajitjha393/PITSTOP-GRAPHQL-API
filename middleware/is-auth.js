73e4ae43c54bb689ea6027d020967e2de3f4beb8
Bishwajit Jha <ajitjha393@gmail.com>
2020-07-17 09:31:04 +0530

    Added m/w for adding auth info in req body

--- middleware/is-auth.js
+++ middleware/is-auth.js
@@ -4,11 +4,8 @@ module.exports = (req, res, next) => {
 	// Extract token from Header
 	const authHeader = req.get('Authorization')
 	if (!authHeader) {
-		const err = new Error(
-			'Authorization header not set, cannot authenticate'
-		)
-		err.statusCode = 401
-		throw err
+		req.isAuth = false
+		return next()
 	}
 
 	const token = authHeader.split(' ')[1]
@@ -18,17 +15,17 @@ module.exports = (req, res, next) => {
 		// This decodes as well as verifies the token
 		decodedToken = jwt.verify(token, 'pitstopSecretKey')
 	} catch (err) {
-		err.statusCode = 500
-		throw err
+		req.isAuth = false
+		return next()
 	}
 
 	if (!decodedToken) {
-		const err = new Error('User is not Authenticated...')
-		err.statusCode = 401
-		throw err
+		req.isAuth = false
+		return next()
 	}
 
 	// Added this userId to the req so that it can be used for authorization afterwards
 	req.userId = decodedToken.userId
+	req.isAuth = true
 	next()
 }
